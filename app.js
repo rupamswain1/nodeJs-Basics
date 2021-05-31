3c4fa1fec706cc420a1e53907c7e1b07046a9461
rupamswain1 <rupamswain1@gmail.com>
2021-06-01 01:24:33 +0530

    response and redirect learned

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const http=require('http');
-
+const fs=require('fs')
 const server=http.createServer((req,res)=>{
     //print request data
-    console.log(req.url,req.method,req.headers);
+        //console.log(req.url,req.method,req.headers);
     //send response
+    const url=req.url;
+    const method=req.method;
+    if(url==='/'){
+        res.setHeader('Content-Type','text/html')
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Submit</button></form></body>')
+        res.write('</html>');
+         return res.end();
+    }
+    if(url==='/message' && method==='POST'){
+        const body=[]
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk)
+        })
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1]
+            fs.writeFileSync('message.txt',message);
+            
+        })
+        
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     res.setHeader('Content-Type','text/html')
-    res.write('<html>');
-    res.write('<body>Node Js is Live</body>')
-    res.write('</html>');
-    res.end();
+        res.write('<html>');
+        res.write('<body>Node Js is Live!! this is not the home page</body>')
+        res.write('</html>');
+        res.end();
 })
 
 server.listen(8000);
